21d5876fd3f12a306922bab0e09806e7a8753575
shreyashristi25 <shreyashristi25@gmail.com>
2026-03-10 16:16:42 +0530

    [Shreya] : Added UC15: N-Tier Architecture Refactoring for Quantity Measurement

--- src/main/java/com/apps/quantitymeasurement/core/WeightUnit.java
+++ src/main/java/com/apps/quantitymeasurement/core/WeightUnit.java
@@ -1,6 +1,6 @@
-package com.apps.quantitymeasurement;
+package com.apps.quantitymeasurement.core;
 
-public enum WeightUnit implements IMeasurable{
+public enum WeightUnit implements IMeasurable {
 
     MILLIGRAM(0.001),
     GRAM(1.0),
@@ -8,7 +8,7 @@ public enum WeightUnit implements IMeasurable{
     POUND(453.592),
     TONNE(1_000_000.0);
 
-    private final double conversionFactor; // conversion to base (grams)
+    private final double conversionFactor;
 
     WeightUnit(double conversionFactor) {
         this.conversionFactor = conversionFactor;
@@ -21,18 +21,16 @@ public enum WeightUnit implements IMeasurable{
 
     @Override
     public double convertToBaseUnit(double value) {
-        double result = value * conversionFactor; 
-        return Math.round(result * 100.0) / 100.0;
+        return value * conversionFactor;
     }
 
     @Override
     public double convertFromBaseUnit(double baseValue) {
-        double result = baseValue / conversionFactor; // from grams
-        return Math.round(result * 100.0) / 100.0; // round
+        return baseValue / conversionFactor;
     }
 
     @Override
     public String getUnitName() {
-        return this.name(); // unit name
+        return name();
     }
 }
