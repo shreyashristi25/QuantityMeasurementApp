1ababe24155c936d67dddbd781df918d19e682a8
shreyashristi25 <shreyashristi25@gmail.com>
2026-03-22 17:30:07 +0530

    [Shreya] : Added feature/UC17-SpringBackend

---
+++ src/main/java/com/app/quantitymeasurement/QuantityMeasurementApplication.java
@@ -0,0 +1,10 @@
+package com.app.quantitymeasurement;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantityMeasurementApplication { // The class name matches your project artifact name
+    public static void main(String[] args) {
+        SpringApplication.run(QuantityMeasurementApplication.class, args);
+    }
+}
---
+++ src/main/java/com/app/quantitymeasurement/core/SupportsArithmetic.java
@@ -0,0 +1,10 @@
+package com.app.quantitymeasurement.core;
+
+
+// Used to define whether arithmetic is supported.
+@FunctionalInterface
+public interface SupportsArithmetic {
+
+    boolean isSupported();
+
+}
