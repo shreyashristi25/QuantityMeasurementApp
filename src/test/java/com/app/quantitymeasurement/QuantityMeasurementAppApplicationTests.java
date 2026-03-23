b7dfb4181c1601486b7dc7b9c2c4c253aa4f5aef
shreyashristi25 <shreyashristi25@gmail.com>
2026-03-23 11:45:20 +0530

    [Shreya] : Upadted  feature/UC17-SpringBackend

--- src/test/java/com/app/quantitymeasurement/QuantityMeasurementAppApplicationTests.java
+++ src/test/java/com/app/quantitymeasurement/QuantityMeasurementAppApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class QuantityMeasurementAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        System.out.println("Spring Boot application started successfully");
+    }
 }
