b5d3b99146e63341504390d8df2621cc727088fc
shreyashristi25 <shreyashristi25@gmail.com>
2026-04-03 14:57:52 +0530

    [Shreya] : Deleted unwanted files

--- src/main/java/com/app/quantitymeasurement/exception/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.app.quantitymeasurement.exception;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
